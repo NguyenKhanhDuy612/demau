cc69f6e01e970e3008d0b6d290373ee2d3649e6b
Admin <Admin@ADMIN>
2022-05-17 00:11:06 +0700

    Add project files.

---
+++ demau/Controllers/SINHVIENsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using demau.Models;
+
+namespace demau.Controllers
+{
+    public class SINHVIENsController : Controller
+    {
+        private KT0720_61133540Entities1 db = new KT0720_61133540Entities1();
+
+        // GET: SINHVIENs
+        public ActionResult GioiThieu()
+        {
+           
+            return View();
+        }
+        public ActionResult TimKiem(string MaSV = "", string HoSV = "")
+        {
+            ViewBag.MaSV = MaSV;
+            ViewBag.HoSV = HoSV;
+            var sINHVIENs = db.SINHVIENs.SqlQuery("SinhVien_TimKiem'" + MaSV + "','" + HoSV + "'" );
+            return View(sINHVIENs.ToList());
+        }
+        public ActionResult Index()
+        {
+            var sINHVIENs = db.SINHVIENs.Include(s => s.LOP);
+            return View(sINHVIENs.ToList());
+        }
+
+        // GET: SINHVIENs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SINHVIEN sINHVIEN = db.SINHVIENs.Find(id);
+            if (sINHVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sINHVIEN);
+        }
+
+        // GET: SINHVIENs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaLop = new SelectList(db.LOPs, "MaLop", "TenLop");
+            return View();
+        }
+
+        // POST: SINHVIENs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaSV,HoSV,TenSV,NgaySinh,GioiTinh,AnhNV,DiaChi,MaLop")] SINHVIEN sINHVIEN)
+        {
+            var imgNV = Request.Files["Avatar"];
+            //Lấy thông tin từ input type=file có tên Avatar
+            string postedFileName = System.IO.Path.GetFileName(imgNV.FileName);
+            //Lưu hình đại diện về Server
+            var path = Server.MapPath("/Images/" + postedFileName);
+            imgNV.SaveAs(path);
+
+            if (ModelState.IsValid)
+            {
+                sINHVIEN.AnhNV = postedFileName;
+                db.SINHVIENs.Add(sINHVIEN);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLop = new SelectList(db.LOPs, "MaLop", "TenLop", sINHVIEN.MaLop);
+            return View(sINHVIEN);
+        }
+
+        // GET: SINHVIENs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SINHVIEN sINHVIEN = db.SINHVIENs.Find(id);
+            if (sINHVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaLop = new SelectList(db.LOPs, "MaLop", "TenLop", sINHVIEN.MaLop);
+            return View(sINHVIEN);
+        }
+
+        // POST: SINHVIENs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaSV,HoSV,TenSV,NgaySinh,GioiTinh,AnhNV,DiaChi,MaLop")] SINHVIEN sINHVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sINHVIEN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaLop = new SelectList(db.LOPs, "MaLop", "TenLop", sINHVIEN.MaLop);
+            return View(sINHVIEN);
+        }
+
+        // GET: SINHVIENs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SINHVIEN sINHVIEN = db.SINHVIENs.Find(id);
+            if (sINHVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sINHVIEN);
+        }
+
+        // POST: SINHVIENs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            SINHVIEN sINHVIEN = db.SINHVIENs.Find(id);
+            db.SINHVIENs.Remove(sINHVIEN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
